f4762459f1e492b22efe10aa9b86b22ae65a5232
Wonsoong__EEE <152298931+ixiwest0@users.noreply.github.com>
2024-10-17 11:29:19 +0900

    Add files via upload

---
+++ LAB 4-5.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("정수를 입력하세요,\n");
+	scanf("%d", &n);
+
+	n += (n % 2 == 0) ? 2 : 1;
+
+	printf("다음 짝수: %d", n);
+}
