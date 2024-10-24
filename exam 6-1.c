65c04de43e9f8d48131668ced32be98e43e145ac
Wonsoong__EEE <152298931+ixiwest0@users.noreply.github.com>
2024-10-24 12:15:44 +0900

    Add files via upload

---
+++ exam 6-1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int n;
+	int i = 1;
+
+	printf("출력하고 싶은 단을 입력하시오: ");
+	scanf("%d", &n);
+
+	while (i <= 9)
+	{
+		printf("%d * %d = %d\n", n, i, n * i);
+		i++;
+	}
+}
