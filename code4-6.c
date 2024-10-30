6d4634f2599b7324ef1b2c1f41be683923a24c2c
Wonsoong__EEE <152298931+ixiwest0@users.noreply.github.com>
2024-10-30 17:04:09 +0900

    Add files via upload

---
+++ code4-6.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("나이를 입력하세요.\n");
+    printf("미성년자면 0, 성인이면 1을 출력\n");
+    scanf("%d", &age);
+    printf("결과: %d", age >= 20);
+}
