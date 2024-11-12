ab55d1b593638bfe94db5f9f8cb2cbd887b70176
Wonsoong__EEE <152298931+ixiwest0@users.noreply.github.com>
2024-11-12 10:06:30 +0900

    Add files via upload

---
+++ exam07_random.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#define SIZE 6
+
+int main()
+{
+	int freq[SIZE] = { 0 };
+	int i;
+	srand((unsigned)time(NULL));
+	for (i = 0; i < 10000; i++)
+		++freq[rand() % 6];
+	printf("====================\n");
+	printf(" ¼ýÀÚ   ºóµµ\n");
+	printf("====================\n");
+
+	for (i = 0; i < SIZE; i++)
+		printf("%3d     %3d\n", i, freq[i]);
+}
