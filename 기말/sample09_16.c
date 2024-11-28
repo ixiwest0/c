3acef7dd6db13563f37b25b4821091ea2b72b314
Wonsoong__EEE <152298931+ixiwest0@users.noreply.github.com>
2024-11-28 12:16:30 +0900

    Add files via upload

---
+++ 기말/sample09_16.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+
+char s[] = "Man is immortal, because he has a soul";
+char seps[] = " .,!\t\n";
+char *token;
+
+int main()
+{
+	token = strtok(s, seps);
+	while (token != NULL)
+	{
+		printf("ÅäÅ«: %s\n", token);
+
+		token = strtok(NULL, seps);
+	}
+}
